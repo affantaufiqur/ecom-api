36c27e340f3f1eee03180731b73b041cb898d1ce
Affan <affantaufiqur@gmail.com>
2024-02-09 18:18:09 +0700

    better variable naming

--- src/types/insert.types.ts
+++ src/types/insert.types.ts
@@ -1,7 +1,7 @@
 import { users } from "../db/schema.js";
 import { createInsertSchema } from "drizzle-zod";
 
-export const insertUser = createInsertSchema(users, {
+export const insertUserSchema = createInsertSchema(users, {
   email: (e) => e.email.email("Invalid Email"),
   password: (e) => e.password.min(6, "Password must be at least 6 characters long"),
 });

84d411d861934e8df2a292ea5d92e1983bf18dd8
Affan <affantaufiqur@gmail.com>
2024-02-10 09:36:18 +0700

    extends request types

--- src/types/insert.types.ts
+++ src/types/insert.types.ts
@@ -1,7 +1,10 @@
 import { users } from "../db/schema.js";
 import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const insertUserSchema = createInsertSchema(users, {
   email: (e) => e.email.email("Invalid Email"),
   password: (e) => e.password.min(6, "Password must be at least 6 characters long"),
 });
+
+export type UserInfo = Omit<z.infer<typeof insertUserSchema>, "password">;

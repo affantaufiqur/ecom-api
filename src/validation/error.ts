c363a739328fa2a01d96c453a8673a199d341eb3
Affan <affantaufiqur@gmail.com>
2024-02-10 08:54:28 +0700

    use callback for handling validation error

--- src/validation/error.ts
+++ src/validation/error.ts
@@ -1,11 +1,11 @@
 import { ZodError } from "zod";
 
-export function handleValidationError(error: ZodError) {
-  return error.issues
-    .map((issue) => {
-      return issue.path.map((path) => {
-        return `${path}: ${issue.message}`;
-      });
-    })
-    .flat();
+export function handleValidationError(cb: (data: { error: string; message: string[] }) => void, error: ZodError) {
+  const errMessage = error.issues.flatMap((issue) => {
+    return issue.path.map((path) => {
+      return `${path}: ${issue.message}`;
+    });
+  });
+  cb({ error: "validation error", message: errMessage });
+  return;
 }

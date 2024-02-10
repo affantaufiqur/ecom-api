0f46ee8b490f6a2e252e21d1f0fe9b7dce140816
Affan <affantaufiqur@gmail.com>
2024-02-10 08:54:19 +0700

    fix import

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import auth from "./src/routes/auth/create.js";
+import auth from "./src/routes/auth.js";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(auth);
+
+app.use("/api/auth", auth);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 export default app;

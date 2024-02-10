32560f9824e50416b6cadf19304f8c0ce31f7d07
Affan <affantaufiqur@gmail.com>
2024-02-10 09:19:45 +0700

    retrieve token example

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,11 @@
+import "dotenv/config";
 import { Router } from "express";
 import { db } from "../config/db.js";
 import { users } from "../db/schema.js";
 import argon2 from "argon2";
 import { insertUserSchema } from "../types/insert.types.js";
 import { handleValidationError } from "../validation/error.js";
+import jwt from "jsonwebtoken";
 
 const app = Router();
 
@@ -24,12 +26,41 @@ app.post("/register", async (req, res) => {
       email,
       password: hashPassword,
     });
-    res.status(200).json({ message: "User created" });
+    res.status(200).json({ message: "User created", token });
     return;
   } catch (err) {
     res.status(500).json({ error: "Something went wrong" });
     return;
   }
+});
+
+app.post("/login", async (req, res) => {
+  const parseUserRequest = insertUserSchema.safeParse(req.body);
+  if (!parseUserRequest.success) {
+    return handleValidationError(({ error, message }) => {
+      return res.status(400).json({ error, message });
+    }, parseUserRequest.error);
+  }
+  return;
+});
+
+// just an example
+app.get("/token", (req, res) => {
+  const bearer = req.headers.authorization;
+  const authToken = bearer?.split(" ")[1];
+  const secret = process.env.JWT_SECRET!;
+  const token = jwt.sign(
+    {
+      email: "golem",
+    },
+    secret,
+    { expiresIn: "1h" },
+  );
+  const decode = jwt.verify(authToken!, secret);
+  const currentTime = new Date();
+  const tokenExpires = decode.exp;
+  const isExpired = currentTime.getTime() < tokenExpires;
+  return res.json({ token, payload: decode, isExpired });
 });
 
 export default app;

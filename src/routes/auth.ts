c7e397a9354bec72cfb06bbdc2ddaab6f3c3c7af
affantaufiqur <affantaufiqur@gmail.com>
2024-02-11 20:08:11 +0700

    handle token exp on current-user

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -47,7 +47,7 @@ app.post("/register", async (req, res) => {
       });
 
       // user will be type of users or undefined
-      // since if insert fail it will immediantly throw
+      // since if insert fail it will immidiately throw
       // we can ignore the undefined
       const { accessToken, refreshToken } = tokenHandler(user!);
       const expiry = Math.floor(Date.now() / 1000) + 7 * 24 * 60 * 60; // Expiry time in Unix timestamp (seconds)
@@ -109,7 +109,7 @@ app.post("/login", async (req, res) => {
           user_id: getUser.id,
           expires_at: new Date(expiry * 1000), // Convert Unix timestamp to JavaScript Date object
         });
-        return;
+        return res.status(200).json({ user: finalUser, access_token: accessToken, refresh_token: refreshToken });
       }
       return res.status(200).json({ user: finalUser, access_token: accessToken, refresh_token: getRefreshToken.token });
     });
@@ -121,17 +121,25 @@ app.post("/login", async (req, res) => {
 });
 
 app.get("/current-user", (req, res) => {
+  // in this endpoint the bearer token that is passed are the access_token
+  // if the access_token is expired then we will get 401
   try {
     const bearer = req.headers.authorization;
     if (!bearer) {
       return res.status(401).json({ error: "Invalid token" });
     }
     const token = bearer.split(" ")[1];
-    const user = jwt.verify(token, process.env.JWT_SECRET!);
-    return res.status(200).json({ user });
+    const user = jwt.verify(token, process.env.JWT_SECRET!) as jwt.JwtPayload;
+    const leeway = Math.floor(Date.now() / 1000) - 60;
+    if (Date.now() + leeway < user.exp! * 1000) {
+      return res.status(401).json({ error: "Token expired, please login again" });
+    }
+    return res.status(200).json(user);
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Something went wrong" });
+    if ((err as { name: string; message: string }).message === "invalid signature") {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+    res.status(500).json({ error: err });
     return;
   }
 });

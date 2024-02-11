bd05388bd85bbdb048dff4533b1e600c01c6a1d7
affantaufiqur <affantaufiqur@gmail.com>
2024-02-11 19:29:34 +0700

    add try catch on migrate

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,15 +2,18 @@ import { migrate } from "drizzle-orm/mysql2/migrator";
 import { createConnection } from "mysql2/promise";
 import { drizzle } from "drizzle-orm/mysql2";
 
-const connection = await createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "ecom-api",
-  port: 3306,
-  multipleStatements: true,
-});
-
-const db = drizzle(connection);
-await migrate(db, { migrationsFolder: "src/drizzle" });
-await connection.end();
+try {
+  const connection = await createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "ecom-api",
+    port: 3306,
+    multipleStatements: true,
+  });
+  const db = drizzle(connection);
+  await migrate(db, { migrationsFolder: "src/drizzle" });
+  await connection.end();
+} catch (err) {
+  console.log(err);
+}

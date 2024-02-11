bec9c47fd08316ae16994849a8c1f91d9889a8f6
Affan <affantaufiqur@gmail.com>
2024-02-11 19:11:42 +0700

    add seed

---
+++ src/seed/auth.seed.ts
@@ -0,0 +1,20 @@
+import { db } from "@/config/db.js";
+import { users } from "@/db/schema.js";
+import argon2 from "argon2";
+
+async function seed() {
+  const email = ["test@me.com", "test2@me.com", "test3@me.com"];
+  try {
+    for (const mail of email) {
+      const hashPassword = await argon2.hash("123456");
+      await db.insert(users).values({
+        email: mail,
+        password: hashPassword,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+await seed();

778d68932f4b7c3a6a1d9e5c8f615be2602e516d
Affan <affantaufiqur@gmail.com>
2024-02-15 21:05:01 +0700

    search on product

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -4,7 +4,7 @@ import { authMiddleware, roleLevelMiddleware } from "@/middlewares/auth.middlewa
 import { ROLE } from "@/utils/shared.js";
 import { handleValidationError } from "@/validation/error.js";
 import { insertProductSchema, updateProductSchema } from "@/validation/product.validation.js";
-import { eq, like } from "drizzle-orm";
+import { eq, like, count } from "drizzle-orm";
 import { Router } from "express";
 
 const app = Router();
@@ -12,26 +12,47 @@ const app = Router();
 app.get("/products", async (req, res) => {
   try {
     const query = req.query;
-    if (!query) {
-      const product = await db.select().from(products).limit(20);
-      if (!product.length) return res.status(404).json({ message: "There are no products in our database :( )" });
-      return res.status(200).json(product);
+    if (isNaN(Number(query?.page)) && query.page) {
+      return res.status(400).json({ message: "Invalid page number" });
     }
-    const { q, page, category }: { category?: string; q?: string; page?: number } = query;
-    const LIMIT = 3;
-    if (!q && category) {
-      return res.json({ message: "gada" });
+    const pageCount = Number(query.page) < 0 ? Number(query.page) * -1 : Number(query.page) || 1;
+    const limit = 3;
+    const offset = (pageCount - 1) * limit;
+
+    let productData;
+    let countProductData;
+
+    if (query.q) {
+      const { q }: { q?: string } = query;
+      productData = await db
+        .select()
+        .from(products)
+        .where(like(products.name, `%${q?.toString()}%`))
+        .limit(limit)
+        .offset(pageCount ? (pageCount - 1) * limit : 0);
+
+      countProductData = await db
+        .select({ value: count() })
+        .from(products)
+        .where(like(products.name, `%${q?.toString()}%`));
+    } else {
+      productData = await db.select().from(products).limit(limit).offset(offset);
+      countProductData = await db.select({ value: count() }).from(products);
     }
 
-    const searchProducts = await db
-      .select()
-      .from(products)
-      .where(like(products.name, `%${q?.toString()}%`))
-      .limit(LIMIT)
-      .offset(page ? (page - 1) * LIMIT : 0);
-    if (!searchProducts.length)
-      return res.status(404).json({ message: "Product that you are looking for does not exist" });
-    return res.status(200).json(searchProducts);
+    const totalCount = countProductData[0].value;
+    const totalPage = Math.ceil(totalCount / limit);
+    const nextPage = pageCount < totalPage ? pageCount + 1 : null;
+
+    return res.json({
+      data: productData,
+      meta: {
+        total: totalCount,
+        current_page: pageCount,
+        next_page: nextPage,
+        total_page: totalPage,
+      },
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Internal server error" });

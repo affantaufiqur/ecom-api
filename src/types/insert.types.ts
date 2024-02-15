17460030aa10703a7481e5227fc61e77ff87858a
Affan <affantaufiqur@gmail.com>
2024-02-15 21:05:01 +0700

    search on product

--- src/types/insert.types.ts
+++ src/types/insert.types.ts
@@ -1,13 +1,14 @@
 import { users } from "../db/schema/schema.js";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
-import { ROLE_NAME } from "@/utils/shared.js";
 
 export const insertUserSchema = createInsertSchema(users, {
   email: (e) => e.email.email("Invalid Email"),
   password: (e) => e.password.min(6, "Password must be at least 6 characters long"),
   role: (e) =>
-    e.role.pipe(z.enum([ROLE_NAME], { required_error: "Role is required", invalid_type_error: "Invalid role" })),
+    e.role.pipe(
+      z.enum(["regular_user", "seller"], { required_error: "Role is required", invalid_type_error: "Invalid role" }),
+    ),
   role_level: (e) => e.role_level.optional(),
 });
 

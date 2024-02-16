bafa32087f327a7f58f0a1c2cf2f3e9984faed71
affantaufiqur <affantaufiqur@gmail.com>
2024-02-16 15:00:54 +0700

    set cart route

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoute from "./src/routes/auth.js";
 import productRoute from "./src/routes/product.js";
+import CartRoute from "./src/routes/cart.js";
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,8 @@ const port = 3000;
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api", productRoute);
+app.use("/api", CartRoute);
+
 app.get("/", (_req, res) => {
   res.json({ message: "Hello World!" });
 });

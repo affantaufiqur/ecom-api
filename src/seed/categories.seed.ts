d9cdde6bed2c3f7a08167ae59ee869c5b0ad6643
Affan <affantaufiqur@gmail.com>
2024-02-17 19:33:02 +0700

    fix import

--- src/seed/categories.seed.ts
+++ src/seed/categories.seed.ts
@@ -1,10 +1,10 @@
 import { db } from "@/config/db.js";
-import { category } from "@/db/schema/schema.js";
+import { categories } from "@/db/schema/schema.js";
 import { PRODUCT_CATEGORIES } from "@/utils/shared.js";
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 (async () => {
   const value = PRODUCT_CATEGORIES.map((category) => ({ name: category }));
-  await db.insert(category).values(value);
+  await db.insert(categories).values(value);
   return;
 })();

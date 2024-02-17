71ca2d46eeb6d3473c0c9eb609c2bb5540724bd7
Affan <affantaufiqur@gmail.com>
2024-02-17 20:12:09 +0700

    better error in middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -7,18 +7,17 @@ import { ROLE } from "@/utils/shared.js";
 function authMiddleware(req: Request, res: Response, next: NextFunction) {
   try {
     const bearer = req.headers.authorization;
-    if (!bearer) return res.status(401).json({ message: "Unauthorized" });
+    if (!bearer) return res.status(401).json({ message: "Please provide token" });
     const token = bearer.split(" ")[1];
     if (!token) return res.status(401).json({ message: "Unauthorized" });
 
-    jwt.verify(token, process.env.JWT_SECRET!, (err, decoded) => {
+    return jwt.verify(token, process.env.JWT_SECRET!, (err, decoded) => {
       if (err) return res.status(401).json({ message: "Unauthorized", error: err.message });
       const token = decoded as jwt.JwtPayload;
       req.user = token.user as UserInfo;
       next();
       return;
     });
-    return;
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Internal server error" });

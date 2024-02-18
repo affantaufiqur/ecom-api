3ca8705738417cbe38f6594e9e35b08c96a0e71b
Affan <affantaufiqur@gmail.com>
2024-02-18 12:33:03 +0700

    order routes

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { authMiddleware } from "@/middlewares/auth.middleware.js";
 import { cart_items, carts, order_items, orders, products } from "@/db/schema/schema.js";
 import { and, eq } from "drizzle-orm";
+import { formatPrice } from "@/utils/shared.js";
 
 const app = Router();
 
@@ -16,7 +17,11 @@ app.get("/order", authMiddleware, async (req, res) => {
     const orderItems = await db.query.order_items.findMany({
       where: and(eq(order_items.cart_id, getOrder.cart_id!), eq(order_items.order_id, getOrder.id)),
     });
-    res.json(orderItems);
+    if (!orderItems.length) return res.status(404).json({ message: "Order item not found" });
+    const mappedPrice = orderItems.map((item) => {
+      return { ...item, price: formatPrice(item.price) };
+    });
+    res.json({ data: mappedPrice });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Internal server error" });
@@ -37,7 +42,8 @@ app.get("/order/:id", authMiddleware, async (req, res) => {
     if (!checkCart) return res.status(404).json({ message: "Order not found, because no order were made" });
     const ifUserValid = orderItems.cart_id === checkCart.id;
     if (!ifUserValid) return res.status(401).json({ message: "Unauthorized" });
-    res.json(orderItems);
+    const formattedPrice = formatPrice(orderItems.price);
+    res.json({ data: { ...orderItems, price: formattedPrice } });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Internal server error" });
@@ -47,21 +53,36 @@ app.get("/order/:id", authMiddleware, async (req, res) => {
 app.post("/order", authMiddleware, async (req, res) => {
   try {
     if (!req.user) return res.status(401).json({ message: "Unauthorized" });
-    const { cart_id } = req.body as { cart_id: string };
+    const { cart_id } = req.body as { cart_id: string | null };
     if (!cart_id) return res.status(400).json({ message: "Invalid cart id" });
     const userId = req.user.id;
 
     const isCartOwnByUser = await db.query.carts.findFirst({
       where: and(eq(carts.user_id, userId), eq(carts.id, cart_id)),
     });
+
     if (!isCartOwnByUser)
       return res
         .status(404)
         .json({ message: "Why are you trying to make order for this cart, this belong to someone else" });
 
+    const getCartItems = await db
+      .select({
+        price: products.price,
+        quantity: cart_items.quantity,
+      })
+      .from(cart_items)
+      .where(eq(cart_items.cart_id, cart_id))
+      .innerJoin(products, eq(cart_items.product_id, products.id));
+
+    if (!getCartItems.length) return res.status(404).json({ message: "Cant make orders, because cart is empty" });
+
+    const calculateTotalPrice = getCartItems.reduce((acc, curr) => acc + curr.price * curr.quantity, 0);
+    const calculateQuantity = getCartItems.reduce((acc, curr) => acc + curr.quantity, 0);
+
     async function getOrderById() {
       return await db.query.orders.findFirst({
-        where: and(eq(orders.user_id, userId), eq(orders.cart_id, cart_id)),
+        where: and(eq(orders.user_id, userId), eq(orders.cart_id, cart_id!)),
       });
     }
 
@@ -75,23 +96,11 @@ app.post("/order", authMiddleware, async (req, res) => {
       await db.insert(order_items).values({
         order_id: getOrderId?.id,
         cart_id: getOrderId?.cart_id,
+        quantity: calculateQuantity,
+        price: calculateTotalPrice,
       });
-      //TODO: also put qty and price here
       return res.json({ message: "Orders placed" });
     }
-
-    const cart = await db.select().from(cart_items).where(eq(cart_items.cart_id, cart_id));
-    if (cart.length === 0) return res.status(404).json({ message: "Cant make orders, because cart is empty" });
-    const getCartItems = await db
-      .select({
-        price: products.price,
-        quantity: cart_items.quantity,
-      })
-      .from(cart_items)
-      .where(eq(cart_items.cart_id, cart[0].id))
-      .innerJoin(products, eq(cart_items.product_id, products.id));
-    const calculateTotalPrice = getCartItems.reduce((acc, curr) => acc + curr.price * curr.quantity, 0);
-    const calculateQuantity = getCartItems.reduce((acc, curr) => acc + curr.quantity, 0);
 
     await db.insert(order_items).values({
       cart_id,
@@ -100,7 +109,7 @@ app.post("/order", authMiddleware, async (req, res) => {
       price: calculateTotalPrice,
     });
 
-    await db.delete(cart_items).where(eq(cart_items.cart_id, cart[0].id));
+    await db.delete(cart_items).where(eq(cart_items.cart_id, cart_id));
 
     return res.status(200).json({ message: "Order placed successfully" });
   } catch (err) {
@@ -109,7 +118,6 @@ app.post("/order", authMiddleware, async (req, res) => {
   }
 });
 
-// illegal
 // app.get("/orders", authMiddleware, async (req, res) => {
 //   const bearer = req.headers.authorization;
 //   const token = bearer.split(" ")[1];
